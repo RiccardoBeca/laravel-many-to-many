316b09bba66d8fc5bbead7ebd5537b442d40aef8
RiccardoBeca <riccardobeca@gmail.com>
2022-07-13 16:18:55 +0100

    api

---
+++ resources/js/App.vue
@@ -0,0 +1,59 @@
+<template>
+<div>
+  <h1>ciao vue</h1>
+
+    <ul>
+      <li v-for="post in posts"
+      :key="post.id">
+        <p>{{ post.title }}</p>
+      </li>
+    </ul>
+
+
+</div>
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data(){
+    
+    return{
+      
+      posts: null
+    }
+
+  },
+
+  methods:{
+
+    getApi(){
+
+        axios.get('/api/posts')
+        .then(response =>{
+
+          this.posts = response.data;
+
+        })
+
+      // console.log('api');
+    }
+
+  },
+
+  mounted() {
+    
+    this.getApi();
+    // console.log(axios);
+
+  },
+}
+</script>
+
+<style>
+
+</style>
